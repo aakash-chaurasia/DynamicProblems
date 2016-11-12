56f08f4b0425709a901b6066aad15a9607c2599d
aakash-chaurasia <aakash.chaurasia91@gmail.com>
2016-11-12 07:04:03 -0700

    Added optimized method

--- Module/src/LongestSequenceInMatrix.java
+++ Module/src/LongestSequenceInMatrix.java
@@ -9,29 +9,29 @@
 12 9 10 9
 7 8 3 2
 6 5 4 10
-
+Ans = 13
 12 9 10
 7 8 3
 6 5 4
-
+Ans = 8
 13 14 15 13
 12 11 16 17
 23 10 15 18
 24 21 20 19
 23 22 21 24
-
+Ans = 15
 13 14 15 13
 12 11 16 17
 23 10 9 18
 24 21 20 19
 23 22 21 24
-
+Ans = 16
 13 14 15 13
 12 11 16 17
 23 10 9 20
 24 21 20 19
 23 22 21 24
-
+Ans = 9
 13 14 18 13
 12 11 18 17
 25 10 9 20
@@ -48,7 +48,33 @@ public class LongestSequenceInMatrix {
         GridMatrix gridMatrix = new GridMatrix();
         int[][] matrix = gridMatrix.GridInput();
         int res = sequenceInMatrix.getMaxSequence(matrix);
+        res = sequenceInMatrix.pullValuesApproach(matrix);
         System.out.println("res = " + res);
+    }
+
+    private int pullValuesApproach(int[][] matrix) {
+        int result = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                DPHelper[i][j] = getDirectionalSequence(matrix, i, j);
+                result = Math.max(result, DPHelper[i][j]);
+            }
+        }
+        return result;
+    }
+
+    private int getDirectionalSequence(int[][] matrix, int i, int j) {
+        if(DPHelper[i][j] == 0) {
+            int curr = 1;
+            int top = i > 0 && matrix[i][j] - matrix[i - 1][j] == 1 ? getDirectionalSequence(matrix, i - 1, j) : 0;
+            int left = j > 0 && matrix[i][j] - matrix[i][j - 1] == 1 ? getDirectionalSequence(matrix, i, j - 1) : 0;
+            int bottom = i < matrix.length - 1 && matrix[i][j] - matrix[i + 1][j] == 1 ? getDirectionalSequence(matrix, i + 1, j) : 0;
+            int right = j < matrix[0].length - 1 && matrix[i][j] - matrix[i][j + 1] == 1 ? getDirectionalSequence(matrix, i, j + 1) : 0;
+            DPHelper[i][j] = maxAllDirection(curr, top, left, bottom, right);
+        }
+        return DPHelper[i][j];
     }
 
     private int getMaxSequence(int[][] matrix) {
